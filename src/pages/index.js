73deb74d15e32c9177be4099e898e728130a6f80
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-08-26 14:54:02 +0200

    metadata strony wstepnie ustawione

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Nav from "../components/Nav";
+import Seo from "../components/Seo";
 
 import "../styles/global.css";
 
 const IndexPage = () => {
   return (
     <>
+      <Seo />
       <Nav></Nav>
     </>
   );
