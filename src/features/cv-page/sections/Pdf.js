322cd4fdf73ae59252b6e44aa81584b3c2fe67e7
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-09-15 20:18:32 +0200

    dodalem cv do strony /cv

--- src/features/cv-page/sections/Pdf.js
+++ src/features/cv-page/sections/Pdf.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { useLanguage } from "@hooks/useLanguage";
 
-import pdf from "../../../assets/images/cv/Wiktor_Rudzki_11k2_L04.pdf";
+import pdfEN from "../../../assets/images/cv/CV_EN.pdf";
+import pdfPl from "../../../assets/images/cv/CV_PL.pdf";
 
-import { cvStyle } from "../styles/pdf.module.css"
+import { cvStyle } from "../styles/pdf.module.css";
 
-const Pdf = () => (
+const Pdf = () => {
+  const { languageState } = useLanguage();
+
+  const languages = {
+    en: pdfEN,
+    pl: pdfPl,
+  };
+
+  return (
     <embed
-      src={pdf}
+      src={languages[languageState.lang]}
       type="application/pdf"
       width="100%"
       className={cvStyle}
     />
-);
+  );
+};
 
 export default Pdf;
